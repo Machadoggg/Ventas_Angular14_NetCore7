cba6f06ebb9a15ea4ea99411582fa327e5c9ac08
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2023-08-07 13:31:05 -0500

    Add Automapper dependency service

--- Ventas_Angular14_NetCore7.IOC/Dependencia.cs
+++ Ventas_Angular14_NetCore7.IOC/Dependencia.cs
@@ -2,9 +2,9 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Ventas_Angular14_NetCore7.DAL.DBContext;
-
-using Ventas_Angular14_NetCore7.DAL.Repositorios.Contrato;
 using Ventas_Angular14_NetCore7.DAL.Repositorios;
+using Ventas_Angular14_NetCore7.DAL.Repositorios.Contrato;
+using Ventas_Angular14_NetCore7.Utility;
 
 namespace Ventas_Angular14_NetCore7.IOC
 {
@@ -13,15 +13,17 @@ namespace Ventas_Angular14_NetCore7.IOC
         public static void InyectarDependencias(this IServiceCollection services, IConfiguration configuration)
         {
             //Dependencia base de datos
-            services.AddDbContext<VentasAngular14Context>(options => {
+            services.AddDbContext<VentasAngular14Context>(options =>
+            {
                 options.UseSqlServer(configuration.GetConnectionString("cadenaSQL"));
             });
 
             //crear dependencia de los repositorios que habiamos creado (para cualquier modelo:  <>)
             services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-            
             //Se especifica el modelo
             services.AddScoped<IVentaRepository, VentaRepository>();
+
+            services.AddAutoMapper(typeof(AutomapperProfile));
         }
     }
 }
