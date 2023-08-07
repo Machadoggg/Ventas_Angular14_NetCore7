373f49fa71348290496e453c4dfb7cd0f5c12387
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2023-08-07 08:33:47 -0500

    Add register sale

--- Ventas_Angular14_NetCore7.DAL/Repositorios/Contrato/IVentaRepository.cs
+++ Ventas_Angular14_NetCore7.DAL/Repositorios/Contrato/IVentaRepository.cs
@@ -3,7 +3,7 @@
 namespace Ventas_Angular14_NetCore7.DAL.Repositorios.Contrato
 {
     //Aqui especificamos que vamos a trabajar con el
-    //modelo de (Venta) en especifica (IGenericRepository<Venta>)
+    //modelo de (Venta) en especifico (IGenericRepository<Venta>)
     public interface IVentaRepository : IGenericRepository<Venta>
     {
         Task<Venta> Registrar(Venta modelo);
---
+++ Ventas_Angular14_NetCore7.DAL/Repositorios/VentaRepository.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Ventas_Angular14_NetCore7.DAL.DBContext;
+using Ventas_Angular14_NetCore7.DAL.Repositorios.Contrato;
+using Ventas_Angular14_NetCore7.Model;
+
+namespace Ventas_Angular14_NetCore7.DAL.Repositorios
+{
+    public class VentaRepository : GenericRepository<Venta>, IVentaRepository
+    {
+        private readonly VentasAngular14Context _dbcontext;
+
+        public VentaRepository(VentasAngular14Context dbcontext) : base(dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+        public async Task<Venta> Registrar(Venta modelo)
+        {
+            Venta ventaGenerada = new Venta();
+
+            ////////////////////////TRANSACTION///////////////////
+            using (var trasction = _dbcontext.Database.BeginTransaction())
+            {
+                try
+                {
+                    //resta el STOCK de cada producto que esta involucrado dentro del detalle de la venta
+                    foreach (DetalleVenta detalleVenta in modelo.DetalleVenta)
+                    {
+                        Producto productoEncontrado = _dbcontext.Productos.Where(p => p.IdProducto == detalleVenta.IdProducto).First();
+
+                        productoEncontrado.Stock = productoEncontrado.Stock - detalleVenta.Cantidad;
+                        _dbcontext.Productos.Update(productoEncontrado);
+                    }
+                    await _dbcontext.SaveChangesAsync();
+
+                    NumeroDocumento correlativo = _dbcontext.NumeroDocumentos.First();
+
+                    correlativo.UltimoNumero = correlativo.UltimoNumero + 1;
+                    correlativo.FechaRegistro = DateTime.Now;
+
+                    _dbcontext.NumeroDocumentos.Update(correlativo);
+                    await _dbcontext.SaveChangesAsync();
+
+                    //Generar formato digitos (0000)
+                    int cantidadDigitos = 4;
+                    string ceros = string.Concat(Enumerable.Repeat("0", cantidadDigitos));
+                    string numeroVenta = ceros + correlativo.UltimoNumero.ToString();//se crea (00001)
+
+                    //obtine los 4 ultimos digitos de atras hacia adelante
+                    //y define donde empieza numeroVenta.Length - cantidadDigitos = (0_0001)
+                    numeroVenta = numeroVenta.Substring(numeroVenta.Length - cantidadDigitos);
+
+                    modelo.NumeroDocumento = numeroVenta;
+
+                    await _dbcontext.Venta.AddAsync(modelo);
+                    await _dbcontext.SaveChangesAsync();
+
+                    ventaGenerada = modelo;
+
+                    trasction.Commit();
+
+                }
+                catch (Exception)
+                {
+                    trasction.Rollback();
+                    throw;
+                }
+                return ventaGenerada;
+            }
+        }
+    }
+}
