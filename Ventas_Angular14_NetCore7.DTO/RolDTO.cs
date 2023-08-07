8a8f522917e30a3e7c7a7fe73ecdc84f5bf23c02
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2023-08-07 11:08:38 -0500

    Clean usings  DTOs

--- Ventas_Angular14_NetCore7.DTO/RolDTO.cs
+++ Ventas_Angular14_NetCore7.DTO/RolDTO.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ventas_Angular14_NetCore7.DTO
+﻿namespace Ventas_Angular14_NetCore7.DTO
 {
     public class RolDTO
     {
