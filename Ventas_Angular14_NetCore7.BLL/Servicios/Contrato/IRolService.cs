b2499cecd8bb8ef8ce6a261b4df112c91d39ec8c
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2023-08-07 14:55:32 -0500

    Add Contract Rol in RolService

---
+++ Ventas_Angular14_NetCore7.BLL/Servicios/Contrato/IRolService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Ventas_Angular14_NetCore7.DTO;
+
+namespace Ventas_Angular14_NetCore7.BLL.Servicios.Contrato
+{
+    public interface IRolService
+    {
+        Task<List<RolDTO>> Lista();
+    }
+}
---
+++ Ventas_Angular14_NetCore7.BLL/Servicios/RolService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AutoMapper;
+using Ventas_Angular14_NetCore7.BLL.Servicios.Contrato;
+using Ventas_Angular14_NetCore7.DAL.Repositorios.Contrato;
+using Ventas_Angular14_NetCore7.DTO;
+using Ventas_Angular14_NetCore7.Model;
+
+
+namespace Ventas_Angular14_NetCore7.BLL.Servicios
+{
+    public class RolService : IRolService
+    {
+        private readonly IGenericRepository<Rol> _rolRepositorio;
+        private readonly IMapper _mapper;
+
+        public RolService(IGenericRepository<Rol> rolRepositorio, IMapper mapper)
+        {
+            _rolRepositorio = rolRepositorio;
+            _mapper = mapper;
+        }
+
+        public async Task<List<RolDTO>> Lista()
+        {
+            try
+            {
+                //Convertir (Rol) a (RolDTO) en forma de lista
+                var listaRoles = await _rolRepositorio.Consultar();
+                return _mapper.Map<List<RolDTO>>(listaRoles);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
