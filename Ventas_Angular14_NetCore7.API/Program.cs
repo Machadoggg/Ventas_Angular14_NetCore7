3e0e92256166da269c2110a62d6ab4cb4009a082
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2023-08-09 01:45:19 -0500

    Add CORS

--- Ventas_Angular14_NetCore7.API/Program.cs
+++ Ventas_Angular14_NetCore7.API/Program.cs
@@ -10,6 +10,14 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.InyectarDependencias(builder.Configuration);
 
+builder.Services.AddCors(options => {
+    options.AddPolicy("NuevaPolitica", app => {
+        app.AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -17,6 +25,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("NuevaPolitica");
 
 app.UseAuthorization();
 
