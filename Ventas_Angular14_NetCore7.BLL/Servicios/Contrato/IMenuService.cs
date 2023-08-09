f34af7624901661af3e85ded89af4ad36737bce9
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2023-08-08 21:19:40 -0500

    Add contract Menu

---
+++ Ventas_Angular14_NetCore7.BLL/Servicios/Contrato/IMenuService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Ventas_Angular14_NetCore7.DTO;
+
+namespace Ventas_Angular14_NetCore7.BLL.Servicios.Contrato
+{
+    public interface IMenuService
+    {
+        Task<List<MenuDTO>> Lista(int idUsuario);
+    }
+}
---
+++ Ventas_Angular14_NetCore7.BLL/Servicios/MenuService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AutoMapper;
+using Ventas_Angular14_NetCore7.BLL.Servicios.Contrato;
+using Ventas_Angular14_NetCore7.DAL.Repositorios.Contrato;
+using Ventas_Angular14_NetCore7.DTO;
+using Ventas_Angular14_NetCore7.Model;
+
+namespace Ventas_Angular14_NetCore7.BLL.Servicios
+{
+    public class MenuService : IMenuService
+    {
+        private readonly IGenericRepository<Usuario> _usuarioRepositorio;
+        private readonly IGenericRepository<MenuRol> _menuRolRepositorio;
+        private readonly IGenericRepository<Menu> _menuRepositorio;
+        private readonly IMapper _mapper;
+
+        public MenuService(IGenericRepository<Usuario> usuarioRepositorio, 
+            IGenericRepository<MenuRol> menuRolRepositorio, 
+            IGenericRepository<Menu> menuRepositorio, 
+            IMapper mapper)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+            _menuRolRepositorio = menuRolRepositorio;
+            _menuRepositorio = menuRepositorio;
+            _mapper = mapper;
+        }
+
+        public async Task<List<MenuDTO>> Lista(int idUsuario)
+        {
+            IQueryable<Usuario> tablaUsuario = await _usuarioRepositorio.Consultar(u => u.IdUsuario == idUsuario);
+            IQueryable<MenuRol> tablaMenuRol = await _menuRolRepositorio.Consultar();
+            IQueryable<Menu> tablaMenu = await _menuRepositorio.Consultar();
+
+            //obtener los menus de acuerdo a el tipo de usuario en base a (idUsuario)
+            try
+            {
+                IQueryable<Menu> tablaResultado = (from u in tablaUsuario
+                                                   join mr in tablaMenuRol on u.IdRol equals mr.IdRol
+                                                   join m in tablaMenu on mr.IdMenu equals m.IdMenu
+                                                   select m).AsQueryable();
+
+                var listaMenus = tablaResultado.ToList();
+                return _mapper.Map<List<MenuDTO>>(listaMenus); 
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
