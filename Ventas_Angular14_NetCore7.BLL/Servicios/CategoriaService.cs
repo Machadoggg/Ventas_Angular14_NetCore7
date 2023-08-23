ee786cba3bc194025948f70fa74e1d4b69c0553e
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2023-08-22 19:51:56 -0500

    Remove AutoMapper references in Categoria service

--- Ventas_Angular14_NetCore7.BLL/Servicios/CategoriaService.cs
+++ Ventas_Angular14_NetCore7.BLL/Servicios/CategoriaService.cs
@@ -1,5 +1,4 @@
-﻿using AutoMapper;
-using Ventas_Angular14_NetCore7.BLL.Servicios.Contrato;
+﻿using Ventas_Angular14_NetCore7.BLL.Servicios.Contrato;
 using Ventas_Angular14_NetCore7.DAL.Repositorios.Contrato;
 using Ventas_Angular14_NetCore7.Model;
 
@@ -8,12 +7,10 @@ namespace Ventas_Angular14_NetCore7.BLL.Servicios
     public class CategoriaService : ICategoriaService
     {
         private readonly IGenericRepository<Categoria> _categoriaRepositorio;
-        private readonly IMapper _mapper;
 
-        public CategoriaService(IGenericRepository<Categoria> categoriaRepositorio, IMapper mapper)
+        public CategoriaService(IGenericRepository<Categoria> categoriaRepositorio)
         {
             _categoriaRepositorio = categoriaRepositorio;
-            _mapper = mapper;
         }
 
         public async Task<List<Categoria>> Lista()
